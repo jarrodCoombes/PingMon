a977cc876007f61eba11f352a5b389252957a0e0
jarrodCoombes <emailjrc+github@gmail.com>
2022-02-21 00:52:21 -0800

    Added name resolution check

--- pingmon.sh
+++ pingmon.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+# Exit 1: No arguments passed in.
+#Exit 2: Not a valid IP or resolvable hostname
+#Exit 3: Unknown error to do with the host command, used to resolve the hostname.
+
+
+
 failed=0
+
+dnsserver="8.8.8.8"  # Specify DNS server
+
 
 function valid_ip()
 {
@@ -19,8 +28,30 @@ function valid_ip()
             && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
         stat=$?
     fi
+    echo "Does not appear to be an IP, tryingt to resolve to an IP."
     return $stat
 }
+
+
+function get_ipaddr 
+{
+# function to get IP address
+  ip_address=""
+  query_type="A"
+  hostname="${1}." #added the . to make it a FQDN
+  # use host command for DNS lookup operations
+  host -t ${query_type}  ${hostname} &>/dev/null ${dnsserver}
+    if [ "$?" -eq "0" ]; then
+      # get ip address
+      ip_address="$(host -t ${query_type} ${hostname} ${dnsserver}| awk '/has.*address/{print $NF; exit}')"
+    else
+      echo "An unknown error has occured"
+      exit 3
+    fi
+# display ip
+ echo $ip_address
+}
+
 
 
 if [ -z "$1" ]
@@ -28,15 +59,23 @@ if [ -z "$1" ]
      echo "Missing Target"
      exit 1
 
-# Todo: Check validity of target
 	else
 		if valid_ip $1 
 		then 
 	    	echo "Valid IP: " $1
 	    else
-			echo "Invalid IP: " $1 
-			echo "Exiting"
-			exit 1
+			address="$(get_ipaddr ${1})"
+			echo $1 " = " $address
+			if [ "$?" -eq "0" ]; then
+    			if [ -n "${address}" ]; then
+    				echo "The adress of the Hostname ${1} is: $address"
+    			else
+    				echo "Address: " $1 " is not a vaiid IP or a resolveable hostname"
+    				exit 2
+    			fi
+  			else
+    			echo "An error occurred"
+  			fi
     	fi
 fi
 
