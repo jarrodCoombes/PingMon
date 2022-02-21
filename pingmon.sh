0768b197f9d110307f814ecdd0d60113d5ff1358
jarrodCoombes <emailjrc+github@gmail.com>
2022-02-21 02:22:35 -0800

    Streamlined some of the code, added a few more comments

--- pingmon.sh
+++ pingmon.sh
@@ -3,13 +3,15 @@
 # Exit 1: No arguments passed in.
 # Exit 2: Not a valid IP or resolvable hostname
 # Exit 3: Unknown error to do with the host command, used to resolve the hostname.
-
+# Exit 9: Something when wrong with the host command
 
 #--------------------------------------------------------------------------------
 # Global Variables
 #--------------------------------------------------------------------------------
 
-
+target=$1
+ping_sleep=5 #Number of seconds between successful pings
+failed_pings=4 #Number of failed pings in a row before logging the target down
 failed=0
 dnsserver="8.8.8.8"  # Specify DNS server
 
@@ -37,28 +39,30 @@ function valid_ip()
             && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
         stat=$?
     fi
-    echo "Does not appear to be an IP, tryingt to resolve to an IP."
     return $stat
 #------------- End function valid_ip -------------
 }
 
-function get_ipaddr 
+function get_ipaddr()
 {
 # Function to convert the hostname to an IP using the host command.
 # Spurce: https://linuxhint.com/resolve_hostname_ip_address_bash_script/
 # To do:
 
-  ip_address=""
-  query_type="A"
-  hostname="${1}." #added the . to make it a FQDN
-  # use host command for DNS lookup operations
+  local ip_address=""
+  local query_type="A"
+  local hostname=$1. # Need to make it a FQDN, so we add a .
+  # Use host command for DNS lookup operations
   host -t ${query_type}  ${hostname} &>/dev/null ${dnsserver}
     if [ "$?" -eq "0" ]; then
       # get ip address
       ip_address="$(host -t ${query_type} ${hostname} ${dnsserver}| awk '/has.*address/{print $NF; exit}')"
+      echo $ip_address
     else
       exit 3
     fi
+    
+    return 0
 #------------- End function get_ipaddress -------------
 }
 
@@ -67,39 +71,41 @@ function get_ipaddr
 #--------------------------------------------------------------------------------
 
 
-if [ -z "$1" ]
+if [ -z "$target" ] # Check yo make sure that an ip or hostname is specified.
    then 
-     echo "Missing Target"
+     echo "ERROR: Missing Target"
      exit 1
 
-	else
-		if valid_ip $1 
-		then 
-	    	echo "Valid IP: " $1
-	    else
-			address="$(get_ipaddr ${1})"
-			echo $address
-			if [ "$?" -eq "0" ]; then
-    			if [ -n "${address}" ]; then
-    				echo "The adress of the Hostname ${1} is: $address"
-    			else
-    				echo "Address: " $1 " is not a vaiid IP or a resolveable hostname"
-    				exit 2
-    			fi
-  			else
-    			echo "An error occurred"
-  			fi
+	else 
+		echo "INFO: Checking to see if the target is a valid IP."
+		if valid_ip $target; then # Target appears to be a valid IP
+		    address=$target
+	    	echo "Valid IP: " $target  
+	    else # Target is not a valid IP or a hostname was used
+		    echo "WARNING: Does not appear to be an IP, tryingt to resolve to an IP."
+			address=$(get_ipaddr $target) 
+    		if [ -n "${address}" ]; then # Hostname was converted to an IP successfully
+    			echo "INFO: Valid HostName: " $target " resolves to: " $address
+    		else # Target was not resolveable and/or the was not an IP address
+    			echo "ERROR: Address: " $target " is not a vaiid IP or a resolveable hostname"
+    			exit 2
+    		fi
     	fi
 fi
 
-while [ $failed -le 4 ]
+# If we get this far, then we have a valid IP work with.
+
+echo 
+echo "Pinging: " $address
+echo 
+while [ $failed -le $failed_pings ]
 do
-	if ping -c 1 -t 1 $1 &> /dev/null
+	if ping -c 1 -t 2 $address &> /dev/null
 		then
-		  # Ping is succesful
-		  echo "Ping Returned"
+		  # Ping is succesful	
+		  echo $(date +"%T") "Ping Returned"
 		  failed=0
-		  sleep 10 #Pause for 10 seconds before trying again.
+		  sleep $ping_sleep #Pause for 10 seconds before trying again.
 	else
 		  # Ping Failed
 		  # echo $failed
